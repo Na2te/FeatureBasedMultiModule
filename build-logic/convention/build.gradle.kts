5ed5d8f4b70e4c9f9347a24d5061c4bd13ab1692
Na2te <111sym@naver.com>
2025-05-08 19:07:22 +0900

    ✨feat: tasks 작성 - build-logic에서 플러그인 유효성 검사를 더 엄격하게 수행하도록 설정

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -11,3 +11,15 @@ kotlin {
         jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
     }
 }
+
+// Gradle 빌드 스크립트에서 특정 작업을 구성하거나 새로운 작업을 정의할 때 사용되는 블록
+tasks {
+    // validatePlugins의 작업에 추가적으로 설정
+    validatePlugins {
+        // 플러그인 유효성 검사를 더 엄격하게 수행하도록 설정
+        enableStricterValidation = true
+        // warning이 발생해도 빌드를 실패시키도록 설정
+        failOnWarning = true
+    }
+}
+
