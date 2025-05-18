e3a5689255681fd069c898fd61efd4af8beb1f1a
Na2te <111sym@naver.com>
2025-05-18 20:07:01 +0900

    ✨feat: AndroidLibraryConventionPlugin 수정 - Serialization Plugin 및 의존성 추가

--- build-logic/convention/src/main/java/com/na2te/convention/plugins/AndroidLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/java/com/na2te/convention/plugins/AndroidLibraryConventionPlugin.kt
@@ -2,21 +2,28 @@ package com.na2te.convention.plugins
 
 import com.android.build.gradle.LibraryExtension
 import com.na2te.convention.configure.configureAndroid
+import com.na2te.convention.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
 
-class AndroidLibraryConventionPlugin : Plugin<Project>{
+class AndroidLibraryConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
-        with(target){
+        with(target) {
             apply(plugin = "com.android.library")
             apply(plugin = "org.jetbrains.kotlin.android")
+            apply(plugin = "org.jetbrains.kotlin.plugin.serialization")
 
             extensions.configure<LibraryExtension> {
                 configureAndroid(this)
                 defaultConfig.targetSdk = 35
                 defaultConfig.testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+            }
+
+            dependencies {
+                "implementation"(libs.findLibrary("kotlinx.serialization.json").get())
             }
         }
     }
